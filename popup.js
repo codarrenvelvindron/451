0fb1c896f3b3ab9017969d5ef959d538d7bcab27
Codarren Velvindron <codarren@hackers.mu>
2017-07-14 12:08:49 +0200

    added specific check for error 451 with output
    Task 1 done

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
 window.onload = function() {	
-	function handleClick(){
+	function handleClick(){ //if button is clicked, do stuff here
 		var Url = document.getElementById("tUrl").value;
-	//when button is clicked, do stuff here
-//window.alert(Url);
 		var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4) {
@@ -12,10 +10,21 @@ xhttp.onreadystatechange = function() {
     "http status code: " + this.status;
 	      document.getElementById("statustext").innerHTML =
     "http status text: " + this.statusText;
+
+	if (this.status == 451){
+		//if error 451, display status
+	document.getElementById("detect").innerHTML =
+    "Censorship detected : Error 451";
+	}
+	else {
+	document.getElementById("detect").innerHTML =
+    "No censorship detected :" + Url + " responded with " + this.status;	
+	}
+	
   }
 };
 xhttp.open("GET", Url, true);
 xhttp.send();
-};
+};//end of handleClick
 	document.getElementById("btnCheck").addEventListener("click",handleClick);
-}
+}//end of window.onload
